9ab96cf596768b5eccb65cf9ea0858468dbdb7c3
drtupper <98119701+drtupper@users.noreply.github.com>
2024-11-08 13:19:45 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/* document.write('Text') displays text on the current page. */
+/* 1.  Use the document.write() method to display "Hello from JavaScript!" on the main page */
+
+
+/* alert('Text') displays text in a pop up window */
+/* 2.  Use the alert() method to make a pop-up that says "Hello from a Pop-Up!" */
+
+
+/* console.log('Text') displays text in the console */
+/* you must open the console to see it */
+/* to open the console, right click, click inspect, then click on console */
+/* 3.  Use the console.log() method to make a it say "Hello from the Console!" in the console*/
+
+
+/* 4. Take a screenshot of your console that shows "Hello from the Console!" */
